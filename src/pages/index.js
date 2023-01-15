b8dff501d0f01e2d47995d4c48f10530fc864cdb
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-01-15 19:33:23 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,16 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import { Heading } from '@chakra-ui/react'
 import Link from 'next/link'
-import { Card, CardHeader, CardBody, Text, StackDivider, Box, Stack } from '@chakra-ui/react'
+import { 
+  Card, 
+  CardHeader, 
+  CardBody, 
+  Text, 
+  StackDivider, 
+  Box, 
+  Stack,
+  SimpleGrid,
+} from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -17,98 +26,125 @@ export default function Home() {
         <Heading as='h2' size='2xl'>Rupee Travel</Heading>
         <Text fontSize='xl' style={{ marginTop: '20px' }}>Budget travel checklist for Indians travelling Vietnam 🇻🇳</Text>
         <div style={{ marginTop: '20px' }}>
-          <Card style={{maxWidth: '500px'}}>
-            <CardHeader>
-              <Heading size='md'>Checklist</Heading>
-            </CardHeader>
 
-            <CardBody>
-              <Stack divider={<StackDivider />} spacing='4'>
-                <Box>
-                  <Link href="/debitcards">
+          <SimpleGrid minChildWidth='350px' spacing='40px'>
+
+            <Card style={{ maxWidth: '500px' }}>
+              <CardHeader>
+                <Heading size='md'>Before you arrive 🇮🇳</Heading>
+              </CardHeader>
+
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing='4'>
+                  <Box>
+                    <Link href="/debitcards">
+                      <Heading size='md'>
+                        Debit Card
+                      </Heading>
+                      <Text pt='2' fontSize='sm'>
+                        Zero forex markup debit card
+                      </Text>
+                    </Link>
+                  </Box>
+                  <Box>
                     <Heading size='md'>
-                      Debit Card
+                      Credit Card
                     </Heading>
                     <Text pt='2' fontSize='sm'>
-                      Zero forex markup debit card
+                      Check out the overview of your clients.
                     </Text>
-                  </Link>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    Credit Card
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    Check out the overview of your clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    E-Visa
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    Flight Booking
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    Travel insurance
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    Luggage
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    Hotel Booking
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    Food & Taxi
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    E-sim & new mobile sim
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-                <Box>
-                  <Heading size='md'>
-                    place to work
-                  </Heading>
-                  <Text pt='2' fontSize='sm'>
-                    See a detailed analysis of all your business clients.
-                  </Text>
-                </Box>
-              </Stack>
-            </CardBody>
-          </Card>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      E-Visa
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      Flight Booking
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      Travel insurance
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      Luggage
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      Hotel Booking
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      eSim
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                </Stack>
+              </CardBody>
+            </Card>
+
+            <Card style={{ maxWidth: '500px', marginTop: '10px' }}>
+              <CardHeader>
+                <Heading size='md'>Arrived 🇻🇳</Heading>
+              </CardHeader>
+
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing='4'>
+                  <Box>
+                    <Heading size='md'>
+                      Food & Taxi
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      Local mobile sim
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Heading size='md'>
+                      Place to work
+                    </Heading>
+                    <Text pt='2' fontSize='sm'>
+                      See a detailed analysis of all your business clients.
+                    </Text>
+                  </Box>
+                </Stack>
+              </CardBody>
+            </Card>
+
+          </SimpleGrid>
+
+
+
         </div>
 
       </main>
