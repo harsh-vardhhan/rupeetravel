37030c73ef55d477a57c7a0e8db93062cf52eece
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-01-28 16:02:35 +0700

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,24 @@
 import Seo from '@/component/seo'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps, canonical }) {
   return (
     <ChakraProvider>
-      <Seo canonical={canonical}/>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-5FW1PNQWL6`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5FW1PNQWL6');
+        `}
+      </Script>
+      <Seo canonical={canonical} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
-
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-5FW1PNQWL6`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-5FW1PNQWL6');
-        `}
-      </Script>
       <body>
         <Main />
         <NextScript />
