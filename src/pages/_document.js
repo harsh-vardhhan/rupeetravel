82887a16c8aefd88b87df30ebf1618c38d761a14
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-01-28 14:41:14 +0700

    gtag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-5FW1PNQWL6`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5FW1PNQWL6');
+        `}
+      </Script>
       <body>
         <Main />
         <NextScript />
