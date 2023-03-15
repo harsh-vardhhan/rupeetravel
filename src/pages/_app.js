7cc078ea71aa791a486ca2ede7677802fa545d5f
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-03-15 19:14:45 +0530

    gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,27 +2,17 @@ import Seo from '@/component/seo'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Script from 'next/script'
+import { GoogleAnalytics } from "nextjs-google-analytics"
 
 export default function App({ Component, pageProps, canonical }) {
   return (
     <ChakraProvider>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-5FW1PNQWL6`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-5FW1PNQWL6');
-        `}
-      </Script>
+      <GoogleAnalytics />
       <Seo canonical={canonical} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
-}
+};
 
 App.getInitialProps = ({ ctx }) => {
   const isProd = process.env.NODE_ENV === "production";
