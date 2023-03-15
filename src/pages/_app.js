bc839fe89f60c18b012d78c0341a24656c229577
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-03-15 19:26:00 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,12 @@
 import Seo from '@/component/seo'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import Script from 'next/script'
 import { GoogleAnalytics } from "nextjs-google-analytics"
 
 export default function App({ Component, pageProps, canonical }) {
   return (
     <ChakraProvider>
-      <GoogleAnalytics />
+      <GoogleAnalytics strategy="lazyOnload"/>
       <Seo canonical={canonical} />
       <Component {...pageProps} />
     </ChakraProvider>
