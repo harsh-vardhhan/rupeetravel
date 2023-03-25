77f90a6d1af79013b268fcf2169819cb41b8ea51
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-03-25 21:03:12 +0530

    currency note

---
+++ src/pages/buy-foreign-currency-notes-online.js
@@ -0,0 +1,102 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import { Heading } from '@chakra-ui/react'
+import {
+    Card,
+    CardBody,
+    Stack,
+    List,
+    Divider,
+    CardFooter,
+    ButtonGroup,
+    Button,
+    Image,
+    SimpleGrid,
+    Tag,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, InfoIcon } from '@chakra-ui/icons'
+import Header from '@/component/header'
+import ListView from '@/component/listview'
+
+const features = [
+    {
+        item: 1,
+        icon: CheckCircleIcon,
+        title: 'Buy USD as an extra emergency fund. You can use it for your other non-Vietnam trips as well',
+        color: 'green.500',
+    },
+    {
+        item: 2,
+        icon: CheckCircleIcon,
+        title: 'BookMyForex is an easy to use online service to get foreign currency',
+        color: 'green.500',
+    },
+    {
+        item: 3,
+        icon: CheckCircleIcon,
+        title: `You can keep the foreign currency if its not more than $2000 per person`,
+        color: 'green.500',
+    },
+    {
+        item: 4,
+        icon: InfoIcon,
+        title: 'Place the order atleast 2 working days before you plan to leave ',
+        color: 'red.400',
+    },
+    {
+        item: 5,
+        icon: InfoIcon,
+        title: 'Ensure you select product type as currency note and NOT travel card',
+        color: 'red.400',
+    }
+]
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Rupee Travel | Buy forex currency notes in India</title>
+                <meta name="description" content="best foreign currency exchange online in India" />
+            </Head>
+            <main className={styles.main}>
+
+                <Header title={'Buy foreign currency notes'} />
+
+                <div style={{ marginTop: '20px' }}>
+                    <SimpleGrid minChildWidth='350px' spacing='40px'>
+                        <Card maxW='sm'>
+                            <CardBody>
+                                <Stack mt='6' spacing='3'>
+                                    <Heading size='md'>Book My Forex <Tag colorScheme='teal'>Personally using</Tag></Heading>
+                                    <Image
+                                        src={'/static/images/currency.webp'}
+                                        alt='foriegn currency notes'
+                                        borderRadius='lg'
+                                        style={{ marginTop: '10px' }}
+                                    />
+                                    <List spacing={3}>
+                                        <ListView features={features} />
+                                    </List>
+                                </Stack>
+                            </CardBody>
+                            <Divider style={{ color: '#e2e8f0' }} />
+                            <CardFooter>
+                                <ButtonGroup spacing='2'>
+                                    <Button
+                                        variant='solid'
+                                        colorScheme='blue'
+                                        onClick={() => window.open('https://www.bookmyforex.com/')}>
+                                        Book My Forex
+                                    </Button>
+                                </ButtonGroup>
+                            </CardFooter>
+                        </Card>
+
+                    </SimpleGrid>
+
+
+                </div>
+            </main>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <Text
           fontSize='xl'
           as='h1'
-          style={{ marginTop: '20px', fontWeight: 600}}>
+          style={{ marginTop: '20px', fontWeight: 600 }}>
           Budget travel checklist for Indians travelling Vietnam 🇻🇳
         </Text>
         <div style={{ marginTop: '20px' }}>
@@ -40,7 +40,7 @@ export default function Home() {
                       <Heading size='md'>
                         Debit Card <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
+                      <Text as='h2' pt='2' fontSize='sm'>
                         Zero forex markup debit card
                       </Text>
                     </Link>
@@ -50,8 +50,18 @@ export default function Home() {
                       <Heading size='md'>
                         Credit Card <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
+                      <Text as='h2' pt='2' fontSize='sm'>
                         Zero forex markup credit card
+                      </Text>
+                    </Link>
+                  </Box>
+                  <Box>
+                    <Link href="/buy-foreign-currency-notes-online">
+                      <Heading size='md'>
+                        Foreign Currency Notes <Tag colorScheme='orange'>Recommended</Tag>
+                      </Heading>
+                      <Text as='h2' pt='2' fontSize='sm'>
+                        Lowest exchange rate foreign currency notes
                       </Text>
                     </Link>
                   </Box>
@@ -60,8 +70,8 @@ export default function Home() {
                       <Heading size='md'>
                         E-Visa <Tag colorScheme='teal'>Mandatory</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
-                        Apply your E-Visa online.
+                      <Text as='h2' pt='2' fontSize='sm'>
+                        Apply E-Visa online.
                       </Text>
                     </Link>
                   </Box>
@@ -70,8 +80,8 @@ export default function Home() {
                       <Heading size='md'>
                         Flight Booking <Tag colorScheme='teal'>Mandatory</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
-                        Scan for the cheapest tickets.
+                      <Text as='h2' pt='2' fontSize='sm'>
+                        Cheapest flights from India to Vietnam.
                       </Text>
                     </Link>
                   </Box>
@@ -80,8 +90,8 @@ export default function Home() {
                       <Heading size='md'>
                         Travel insurance <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
-                        Safeguard your journey.
+                      <Text as='h2' pt='2' fontSize='sm'>
+                        Best travel insurance for Indians
                       </Text>
                     </Link>
                   </Box>
@@ -90,7 +100,7 @@ export default function Home() {
                       <Heading size='md'>
                         Luggage <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
+                      <Text as='h2' pt='2' fontSize='sm'>
                         Quick travel hack for luggage.
                       </Text>
                     </Link>
@@ -100,8 +110,8 @@ export default function Home() {
                       <Heading size='md'>
                         Hotel Booking <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
-                        Book hotel directly.
+                      <Text as='h2' pt='2' fontSize='sm'>
+                        Book Vietnam hotel directly.
                       </Text>
                     </Link>
                   </Box>
@@ -110,8 +120,8 @@ export default function Home() {
                       <Heading size='md'>
                         eSim <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
-                        Get eSim for single sim phone.
+                      <Text as='h2' pt='2' fontSize='sm'>
+                        Get Vietnam eSim for single sim phone.
                       </Text>
                     </Link>
                   </Box>
@@ -131,7 +141,7 @@ export default function Home() {
                       <Heading size='md'>
                         Food & Taxi <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
+                      <Text as='h2' pt='2' fontSize='sm'>
                         Download the Zomato + Uber of Vietnam.
                       </Text>
                     </Link>
@@ -141,7 +151,7 @@ export default function Home() {
                       <Heading size='md'>
                         Local mobile SIM card <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
+                      <Text as='h2' pt='2' fontSize='sm'>
                         Get local Vietnam SIM card.
                       </Text>
                     </Link>
@@ -151,7 +161,7 @@ export default function Home() {
                       <Heading size='md'>
                         Place to work <Tag colorScheme='red'>Remote work</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
+                      <Text as='h2' pt='2' fontSize='sm'>
                         For remote workers.
                       </Text>
                     </Link>
@@ -161,7 +171,7 @@ export default function Home() {
                       <Heading size='md'>
                         ATM cash withdrawal  <Tag colorScheme='orange'>Recommended</Tag>
                       </Heading>
-                      <Text pt='2' fontSize='sm'>
+                      <Text as='h2' pt='2' fontSize='sm'>
                         zero forex ATM
                       </Text>
                     </Link>
