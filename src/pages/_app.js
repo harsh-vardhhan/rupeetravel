6a43e81d7fabd1a4781ddf47925163d398d280a6
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-04-04 13:36:33 +0700

    remove analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,9 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { GoogleAnalytics } from "nextjs-google-analytics"
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <GoogleAnalytics strategy="lazyOnload"/>
       <Component {...pageProps} />
     </ChakraProvider>
   )
