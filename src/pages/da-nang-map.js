13d4864c9a286bd237b1143f9576a57c01e0986d
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2023-06-28 08:55:08 +0530

    Update da-nang-map.js

--- src/pages/da-nang-map.js
+++ src/pages/da-nang-map.js
@@ -2,7 +2,7 @@ import Seo from '@/component/seo'
 import styles from '@/styles/Home.module.css'
 import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
 import Header from "@/component/header";
-export default function Home() {
+export default function Home({ deviceType }) {
 
     const { rive, RiveComponent } = useRive({
         src: "map.riv",
@@ -12,21 +12,58 @@ export default function Home() {
     });
 
     const bumpInput = useStateMachineInput(rive, "toggle-dragon-bridge", "hover-on", false);
-    console.log(bumpInput)
-    return (
-        <>
-            <Seo
-                title="Rupee Travel | Da Nang Map"
-                description="Best places to visit in Da Nang"
-                canonical={"https://www.rupeetravel.com/da-nang-map"}
-            />
-            <main className={styles.main}>
-                <Header title={"Da Nang Map"} />
-                <RiveComponent
-                    style={{ height: "600px" }}
-                    onClick={() => bumpInput.value = !bumpInput.value}
+
+    if (deviceType === 'mobile') {
+        return (
+            <>
+                <Seo
+                    title="Rupee Travel | Da Nang Map"
+                    description="Best places to visit in Da Nang"
+                    canonical={"https://www.rupeetravel.com/da-nang-map"}
                 />
-            </main>
-        </>
-    )
+                <main className={styles.main}>
+                    <Header title={"Da Nang Map"} />
+                    <RiveComponent
+                        style={{ 
+                            height: "476px", 
+                            width: "381px",
+                            WebkitTransform: "rotate(90deg)",
+                        }}
+                        onClick={() => bumpInput.value = !bumpInput.value}
+                    />
+                </main>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <Seo
+                    title="Rupee Travel | Da Nang Map"
+                    description="Best places to visit in Da Nang"
+                    canonical={"https://www.rupeetravel.com/da-nang-map"}
+                />
+                <main className={styles.main}>
+                    <Header title={"Da Nang Map"} />
+                    <RiveComponent
+                        style={{ height: "600px"}}
+                        onClick={() => bumpInput.value = !bumpInput.value}
+                    />
+                </main>
+            </>
+        )
+    }
+
+}
+
+export async function getServerSideProps(context) {
+    const UA = context.req.headers['user-agent'];
+    const isMobile = Boolean(UA.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+    ))
+
+    return {
+        props: {
+            deviceType: isMobile ? 'mobile' : 'desktop'
+        }
+    }
 }
