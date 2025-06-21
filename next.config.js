fed3c4d6769378f0d7f3415b85240d1b53add5c5
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-06-21 23:30:09 +0530

    remove unused packages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    runtime: 'experimental-edge',  // Ensure Edge runtime is enabled
-    nodejs_compat: true, // Enable Node.js compatibility
-  },
   reactStrictMode: true,
   transpilePackages: ["react-tweet"],
   images: {
-    unoptimized: true, //
+    unoptimized: true,
     formats: ["image/webp"],
   },
 };
