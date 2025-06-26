6e85842550460ce3abdf2eb49fd7f21e148c9203
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-06-26 11:23:38 +0530

    Update seo.js

--- src/component/seo.js
+++ src/component/seo.js
@@ -17,56 +17,24 @@ export default function Seo({
     <Head>
       <title key="title">{title}</title>
       <meta name="description" content={description} />
-      <meta key="og_type" property="og:type" content={ogType} />
-      <meta key="og_title" property="og:title" content={title} />
-      <meta key="og_description" property="og:description" content={description} />
-      <meta key="og_locale" property="og:locale" content="en_IE" />
-      <meta key="og_site_name" property="og:site_name" content={siteName} />
-      <meta key="og_url" property="og:url" content={canonical ?? DOMAIN} />
-      <meta key="og_site_name" property="og:site_name" content={siteName} />
-      <meta
-        key="og_image"
-        property="og:image"
-        content={ogImage ?? DEFAULT_OG_IMAGE}
-      />
-      <meta
-        key="og_image:alt"
-        property="og:image:alt"
-        content={`${title} | ${siteName}`}
-      />
-      <meta key="og_image:width" property="og:image:width" content="1200" />
-      <meta key="og_image:height" property="og:image:height" content="630" />
+      <meta property="og:type" content={ogType} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:locale" content="en_IN" />
+      <meta property="og:site_name" content={siteName} />
+      <meta property="og:url" content={canonical ?? DOMAIN} />
+      <meta property="og:image" content={ogImage ?? DEFAULT_OG_IMAGE} />
+      <meta property="og:image:alt" content={`${title} | ${siteName}`} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/png" />
       <meta name="robots" content="index,follow" />
-      <meta
-        key="twitter:image"
-        name="twitter:image"
-        content={ogImage ?? DEFAULT_OG_IMAGE}
-      />
-      <meta
-        key="twitter:card"
-        name="twitter:card"
-        content="summary_large_image"
-      />
-      <meta
-        key="twitter:site"
-        name="twitter:site"
-        content={twitterHandle}
-      />
-      <meta
-        key="twitter:creator"
-        name="twitter:creator"
-        content={twitterHandle}
-      />
-      <meta
-        key="twitter:title"
-        property="twitter:title"
-        content={title}
-      />
-      <meta
-        key="twitter:description"
-        property="twitter:description"
-        content={description}
-      />
+      <meta name="twitter:image" content={ogImage ?? DEFAULT_OG_IMAGE} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={twitterHandle} />
+      <meta name="twitter:creator" content={twitterHandle} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
       <link rel="canonical" href={canonical ?? DOMAIN} />
       <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
