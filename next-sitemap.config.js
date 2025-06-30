62edf9169aa41aaacc6a246bd7314d77fadfe4b4
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-06-30 10:25:09 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,35 @@ module.exports = {
   exclude: ["/404"],
   generateRobotsTxt: true,
   generateIndexSitemap: false,
+  additionalPaths: async () => {
+    const result = [
+      {
+        loc: '/bangalore-to-vietnam-flight',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/mumbai-to-vietnam-flight',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/kolkata-to-vietnam-flight',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/new-delhi-to-vietnam-flight',
+        changefreq: 'daily',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+    ];
+    return result;
+  },
   robotsTxtOptions: {
     policies: [
       {
