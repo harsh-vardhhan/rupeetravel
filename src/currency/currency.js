b1a7444d6e0f829adf3e044779676fb560cc10f9
Harsh Vardhan <harsh.vardhan.611@gmail.com>
2025-07-04 00:33:58 +0530

    Update currency.js

--- src/currency/currency.js
+++ src/currency/currency.js
@@ -1,8 +1,9 @@
-export const USDINRMarketRate = 87.80
-export const USDINRBookMyForexRate = 86.96
-
-export const INRVNDMarketRate = 288.21
-export const INRVNDBookMyForexRate = 217
-export const VNDINRMarketRate = 0.0035
-
-export const IDRINRMarketRate = 0.0054
+// USD exchange rate
+export const USDINRMarketRate = 85.41
+export const USDINRBookMyForexRate = 86.4081
+// VND exchange rate
+export const INRVNDMarketRate = 306.77
+export const INRVNDBookMyForexRate = 270.27
+export const VNDINRMarketRate = 0.0033
+ // IDR exchange rate
+export const IDRINRMarketRate = 0.0053
